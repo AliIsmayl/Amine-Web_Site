1e8eec8177128c2fe96094a33c58049b4a40c799
MatanatAdigozelova <adigozelovamata@gmail.com>
2024-05-11 00:21:53 +0400

    about components

---
+++ Amina Web Site/src/Components/AboutComponents/OurHistory/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const OurHistory = () => {
+  return (
+    <div id='about-us'>
+<div className="tittle">
+  <h3>Yaranma tarixi</h3>
+</div>
+<div className="container">
+  <div className="content">
+<p>
+Azərbaycan o nadir ölkələrdəndir ki, qədim və zəngin ənənələri olan güləşi sadəcə idman, mübarizə növü deyil, həm də milliliyin, ruhun, mənəvi dəyərlərin, ədalət və haqqın tərənnümçüsüdür. Tarixən Şərqlə Qərb arasında körpü olmuş Odlar yurdunda bu bəşəri sərvəti qorumaqda güləşin əvəzsiz rolu və yeri var. Digər növlərlə müqayisədə, onlara daha böyük hüsn-rəğbətin və məhəbbətin bir kökü də buradan gəlir. Ancaq xalq dünyalar qədər sevdiyi bahadırlarına qarşı həmişə tələbkar olub, onlardan böyük fədakarlıq gözləyib. Çünki bu diyarda bir pəhləvan qələbəsi ilə bir orduya bərabər iş də görə bilir, onu sabaha – daha böyük zəfərlərə səsləyir. <br />
+Sovetlərdə o qədər də məşhur olmayan milli idman növləri, çağdaş dönəmdə sürətlə inkişaf etməyə başlayıb. Əlbəttə, müstəqillik qazandıqdan sonra Azərbaycan, yeni addımlar atan hər bir ölkə kimi, bir çox sahələrdə özünü tanıtmağa ehtiyac duyurdu. Sözsüz ki, bu mənada idmanın və onun tərənnümçülük xüsusiyyətlərinin rolu danılmazdır. Təbii, bunun üçün, ilk növbədə, ölkənin simasına çevrilən növlərə dəstək verilməsi vacibdir. Bunu, ilk olaraq, həmişə düşünülmüş siyasəti, müdrikliyi, səs-səda doğuran addımları ilə seçilən Xanlar Qurbanov etdi. Təcrübəli müəllim yeni yaranan Azərbaycanı dünya idman aləminə tanıtdırmaq üçün, milli idman növləri assosiasiyasının yaradılmasını lazım bildi.
+
+Beləliklə, AMİNA-nın əsası qoyuldu: Azərbaycan SSR nazirlər sovetinin 6 iyun 1990-cı il 246 №li qararına müvafiq olaraq, assosiasiya iştirakçılarının təsis konfransının 25 aprel qərarı ilə yaradıldı. AMİNA nizamnaməyə  uyğun, respublikada milli idman növlərinin inkişafını həyata keçirən və idarə edən xeyriyyəçi, könüllü və ictimai idman təşkilatıdır. Assosiasiya hüquqi şəxs olaraq öz möhürü, bankda hesabı, ştamp və atributikasına malikdir. Birliyə 5 milli idman növü – güləş, zorxana, nərd, atüstü və  mütəhərrik oyunlar daxildir. AMİNA onun üzərinə düşən vəziflərlə birlikdə aşağıdakı funksiyaları yerinə yetirir:
+</p>
+</div>
+</div>
+
+    </div>
+  )
+}
+
+export default OurHistory
