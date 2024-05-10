3d46a098dc5591c409ab811bfb79375c6b37278e
MatanatAdigozelova <adigozelovamata@gmail.com>
2024-05-10 23:02:43 +0400

    about cards

--- Amina Web Site/src/App.jsx
+++ Amina Web Site/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter , Route, Routes} from "react-router-dom";
 import HomePage from './Pages/HomePage';
 import Navbar from './LayOut/Navbar/Navbar';
+import AboutUs from './Pages/AboutUs/AboutUs';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path="/" element={<HomePage/>} /> 
+          <Route path="/about" element={<AboutUs/>} /> 
         </Routes>
       </BrowserRouter>
     </>
---
+++ Amina Web Site/src/component/AboutCard/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./index.scss"
+const AboutCard = () => {
+  return (
+    <div className='about-card'>
+      <div className="image">
+        <img src="https://amina-azif.az/wp-content/uploads/2023/05/2b374c0e-2f78-4b3c-a24c-3d4effd3f919-480x480-1.jpg" alt="" />
+      </div>
+      <div className="content">
+        <h4>Prezident -
+Kazım Qurbanov</h4>
+<div className='line'></div>
+<p>
+Kazım Xanlar oğlu Qurbanov
+</p>
+      </div>
+    </div>
+  )
+}
+
+export default AboutCard
