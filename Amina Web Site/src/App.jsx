63a3f6517af406cce18118fe7454575b778c7991
MatanatAdigozelova <adigozelovamata@gmail.com>
2024-05-12 19:29:40 +0400

    some changes

--- Amina Web Site/src/App.jsx
+++ Amina Web Site/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter , Route, Routes} from "react-router-dom";
 import HomePage from './Pages/HomePage';
 import Navbar from './LayOut/Navbar/Navbar';
 import Footer from './LayOut/Footer/Footer';
-import AboutUs from './Pages/AboutUs/AboutUs';
 import AboutRehberlik from './Pages/AboutRehberlik';
 import AboutHistory from './Pages/AboutHistory';
 import AboutCommitet from './Pages/AboutCommitet';
@@ -17,7 +16,6 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path="/" element={<HomePage/>} /> 
-          <Route path="/about" element={<AboutUs/>} /> 
           <Route path="/rehberlik" element={<AboutRehberlik/>} /> 
           <Route path="/tariximiz" element={<AboutHistory/>} /> 
           <Route path="/icraiyye-comitesi" element={<AboutCommitet/>} /> 
