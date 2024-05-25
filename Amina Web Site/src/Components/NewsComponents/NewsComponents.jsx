9d63feb413867171b3102e22acdb4869836c1a72
Alismayil <ali.ismayil.681@gmail.com>
2024-05-25 13:17:14 +0400

    neyse

---
+++ Amina Web Site/src/Components/NewsComponents/NewsComponents.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import './NewsComponents.scss'
+
+import { MdKeyboardArrowRight } from "react-icons/md";
+function NewsCard() {
+    return (
+        <div className='newsPage'>
+            <div className="newsCard">
+                <div className="imageBox">
+                    <img src="https://amina-azif.az/wp-content/uploads/2024/04/436347902_17997376283545480_9076048081358952771_n-1280x810.jpg" alt="" />
+                    <div className="timeBox">
+                        <div className="timeBoxHover">
+                            <p>10 <span>MAR</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="textBox">
+                    <div className="upText">
+                        <span></span>
+                        <p>Blog</p>
+                    </div>
+                    <h1>Azərbaycan Alpaqut</h1>
+                    <h6>   Bakı Şəhər Gənclər və İdman idarəsinin rəhbərliyi ilə Azərbaycan Milli İdman Növləri</h6>
+                    <h3>Read More <MdKeyboardArrowRight /></h3>
+                </div>
+            </div>
+            <div className="newsCard">
+                <div className="imageBox">
+                    <img src="https://amina-azif.az/wp-content/uploads/2024/04/436347902_17997376283545480_9076048081358952771_n-1280x810.jpg" alt="" />
+                    <div className="timeBox">
+                        <div className="timeBoxHover">
+                            <p>10 <span>MAR</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="textBox">
+                    <div className="upText">
+                        <span></span>
+                        <p>Blog</p>
+                    </div>
+                    <h1>Azərbaycan Alpaqut</h1>
+                    <h6>   Bakı Şəhər Gənclər və İdman idarəsinin rəhbərliyi ilə Azərbaycan Milli İdman Növləri</h6>
+                    <h3>Read More <MdKeyboardArrowRight /></h3>
+                </div>
+            </div>
+            <div className="newsCard">
+                <div className="imageBox">
+                    <img src="https://amina-azif.az/wp-content/uploads/2024/04/436347902_17997376283545480_9076048081358952771_n-1280x810.jpg" alt="" />
+                    <div className="timeBox">
+                        <div className="timeBoxHover">
+                            <p>10 <span>MAR</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="textBox">
+                    <div className="upText">
+                        <span></span>
+                        <p>Blog</p>
+                    </div>
+                    <h1>Azərbaycan Alpaqut</h1>
+                    <h6>   Bakı Şəhər Gənclər və İdman idarəsinin rəhbərliyi ilə Azərbaycan Milli İdman Növləri</h6>
+                    <h3>Read More <MdKeyboardArrowRight /></h3>
+                </div>
+            </div>
+            <div className="newsCard">
+                <div className="imageBox">
+                    <img src="https://amina-azif.az/wp-content/uploads/2024/04/436347902_17997376283545480_9076048081358952771_n-1280x810.jpg" alt="" />
+                    <div className="timeBox">
+                        <div className="timeBoxHover">
+                            <p>10 <span>MAR</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="textBox">
+                    <div className="upText">
+                        <span></span>
+                        <p>Blog</p>
+                    </div>
+                    <h1>Azərbaycan Alpaqut</h1>
+                    <h6>   Bakı Şəhər Gənclər və İdman idarəsinin rəhbərliyi ilə Azərbaycan Milli İdman Növləri</h6>
+                    <h3>Read More <MdKeyboardArrowRight /></h3>
+                </div>
+            </div>
+            <div className="newsCard">
+                <div className="imageBox">
+                    <img src="https://amina-azif.az/wp-content/uploads/2024/04/436347902_17997376283545480_9076048081358952771_n-1280x810.jpg" alt="" />
+                    <div className="timeBox">
+                        <div className="timeBoxHover">
+                            <p>10 <span>MAR</span></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="textBox">
+                    <div className="upText">
+                        <span></span>
+                        <p>Blog</p>
+                    </div>
+                    <h1>Azərbaycan Alpaqut</h1>
+                    <h6>   Bakı Şəhər Gənclər və İdman idarəsinin rəhbərliyi ilə Azərbaycan Milli İdman Növləri</h6>
+                    <h3>Read More <MdKeyboardArrowRight /></h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewsCard
---
+++ Amina Web Site/src/Components/NewsComponents/NewsDetailPage/NewsDetailPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './NewsDetailPage.scss'
+import { CgTime } from "react-icons/cg";
+import { HiMiniUserCircle } from "react-icons/hi2";
+
+function NewsDetailPage() {
+    return (
+        <div className='newsDetailPage'>
+            <div className="upBox">
+                <div className="imageBox">
+                    <img src="https://metbuat.az/images/metbuat/images_o/1423892.jpg" alt="" />
+                </div>
+                <div className="textBox">
+                    <div className="nothing">
+                        <span></span>
+                        <p>Blog</p>
+                    </div>
+                    <h1>Mas-reslinq idman növü üzrə ölkə birinciliyi və çempionatı keçirildi</h1>
+                    <div className='littleInform'>
+                        <div className="littleBox">
+                            <CgTime />
+                            <p>March 27, 2024</p>
+                        </div>
+                        <div className="littleBox">
+                            <HiMiniUserCircle />
+                            <p>by Ali</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NewsDetailPage
