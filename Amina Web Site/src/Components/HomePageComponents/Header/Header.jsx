ee94261b815573b63801dfede3bd9b19ca685a47
Alismayil <ali.ismayil.681@gmail.com>
2024-06-29 16:50:44 +0400

    neyse

--- Amina Web Site/src/Components/HomePageComponents/Header/Header.jsx
+++ Amina Web Site/src/Components/HomePageComponents/Header/Header.jsx
@@ -25,26 +25,29 @@ function Header() {
                             <p>Azərbaycan Milli Idman Növləri Assosiasiyası veb saytına xoş gəlmisiniz...</p>
                         </div>
                         <div className="rightBox" style={{ display: 'flex', justifyContent: "center", alignItems: "center" }}>
-                            <img style={{ maxWidth: "70%" , borderRadius:"0px"}} src="https://api.mys.gov.az/api/v1/Storages/1919e359-d7c9-4827-8875-b6dd9c7d1f0b" alt="" />
+                            <img style={{ maxWidth: "70%", borderRadius: "0px" }} src="https://api.mys.gov.az/api/v1/Storages/1919e359-d7c9-4827-8875-b6dd9c7d1f0b" alt="" />
                         </div>
                     </div>
                 </SwiperSlide>
                 {
-                    NewsCart && NewsCart.map((item, index) => (
-                        <SwiperSlide>
-                            <div className="swiperBox" key={index}>
-                                <div className="leftBox">
-                                    <p>{item.title}</p>
-                                    <Link to={`news/${item.name}`}>
-                                        <button>Daha Ətraflı </button>
-                                    </Link>
+                    NewsCart && NewsCart
+                        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                        .slice(0, 4)
+                        .map((item, index) => (
+                            <SwiperSlide>
+                                <div className="swiperBox" key={index}>
+                                    <div className="leftBox">
+                                        <p>{item.title}</p>
+                                        <Link to={`news/${item.name}`}>
+                                            <button>Daha Ətraflı </button>
+                                        </Link>
+                                    </div>
+                                    <div className="rightBox">
+                                        <img src={item.image1} alt="" />
+                                    </div>
                                 </div>
-                                <div className="rightBox">
-                                    <img src={item.image1} alt="" />
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))
+                            </SwiperSlide>
+                        ))
                 }
             </Swiper>
         </header>
