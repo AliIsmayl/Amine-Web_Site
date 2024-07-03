47132b2724e12c153a36693d65608925c2c86833
Alismayil <ali.ismayil.681@gmail.com>
2024-07-03 15:25:54 +0400

    header problem

--- Amina Web Site/src/Components/HomePageComponents/Header/Header.jsx
+++ Amina Web Site/src/Components/HomePageComponents/Header/Header.jsx
@@ -19,16 +19,7 @@ function Header() {
     return (
         <header>
             <Swiper navigation={true} loop={true} modules={[Navigation]} className="mySwiper">
-                {/* <SwiperSlide>
-                    <div className="swiperBox" >
-                        <div className="leftBox">
-                            <p>Azərbaycan Milli İdman Növləri Assosiasiyası veb saytına xoş gəlmisiniz...</p>
-                        </div>
-                        <div className="rightBox" style={{ display: 'flex', justifyContent: "center", alignItems: "center" }}>
-                            <img style={{ maxWidth: "70%", borderRadius: "0px" }} src="https://api.mys.gov.az/api/v1/Storages/1919e359-d7c9-4827-8875-b6dd9c7d1f0b" alt="" />
-                        </div>
-                    </div>
-                </SwiperSlide> */}
+                <SwiperSlide style={{ display: "none" }}></SwiperSlide>
                 {
                     NewsCart && NewsCart
                         .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
--- Amina Web Site/src/Pages/HomePage.jsx
+++ Amina Web Site/src/Pages/HomePage.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import Inform from '../Components/HomePageComponents/InformComponents/Inform'
 import Medal from '../Components/HomePageComponents/MedalComponents/Medal'
 import Partner from '../Components/HomePageComponents/PartnerComponents/Partner'
-import Header from '../Components/HomePageComponents/Header/Header'
 import NotMean from '../Components/NotMean/NotMean'
 import Calendar from '../Components/HomePageComponents/Calendar/Calendar'
 import { Helmet } from 'react-helmet-async'
+import Header from '../Components/HomePageComponents/Header/Header'
 
 function HomePage() {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Ana səhifə</title>
-        </Helmet>
-    <NotMean/>
+      </Helmet>
+      <NotMean />
       <Header />
       <Inform />
       <Medal />
-      <Calendar/>
+      <Calendar />
       <Partner />
     </>
   )
